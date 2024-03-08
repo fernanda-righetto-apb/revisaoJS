5f27ef6dda905200f8bf75620f2a642b87a2911c
fernanda-righetto-apb <fernanda.guaragni.estudante@petrobras.com.br>
2024-03-08 15:52:00 -0300

    subir revisão JS

---
+++ type-booleano.js
@@ -0,0 +1,13 @@
+let cidade = "Campo Largo";
+
+cidade = cidade.toLowerCase();
+
+let cidadeNatal = "campo largo";
+
+cidadeNatal = cidadeNatal.toLowerCase();
+
+console.log(cidade===cidadeNatal);
+
+//mesmo tipo e mesmo conteúdo (===)
+// const - valor não se altera
+// let - valor se altera
---
+++ type-string.js
@@ -0,0 +1,12 @@
+const nomeAluna = "Rebeca";
+const maeAluna = " Julia";
+
+console.log(nomeAluna);
+
+const idadeAluna = 21;
+
+console.log(nomeAluna*idadeAluna);
+
+//podemos concatenar strings e isso é, de certa forma, uma operação entre strings.
+
+console.log(nomeAluna+maeAluna);
